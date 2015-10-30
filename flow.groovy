626fe6cbfc75bea2a71507bce2862ef8773ed7b0
hi25.shin <hi25.shin@samsung.com>
2015-10-30 13:57:06 +0900

    add comment

--- flow.groovy
+++ flow.groovy
@@ -1,6 +1,15 @@
 
 // https://github.com/jenkinsci/workflow-plugin/blob/master/TUTORIAL.md
 
+/* Jenkins job configuration
+
+node {
+  git url: 'https://github.com/anyone-can-test/jenkins-workflow-test.git'
+  load 'flow.groovy'
+}()
+*/
+
+
 { ->
   node() {
     hello 'world'
